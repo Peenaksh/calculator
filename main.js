40e71c22c695cffbab5a6e65a6cdf0021bbcb7f9
Peenaksh <85746966+Peenaksh@users.noreply.github.com>
2022-12-08 17:31:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+
+num1 = prompt("ENTER A NUMBER");
+mun2 = prompt("ENTER A SECOND NUMBER "); 
+var w = num1 + mun2 ; 
+var x = num1 - mun2; 
+var y = num1 / mun2 ; 
+var z = num1 ** mun2 ; 
+
+function addition() {
+    console.log(w)
+ document.getElementById("sum").innerHTML = "the sum is " +w;
+ console.log(x)
+ document.getElementById("mul").innerHTML = "the difference is " +x;
+ console.log(y)
+ document.getElementById("divid").innerHTML = "the quotent is " +y ;
+ console.log(z)
+ document.getElementById("exxp").innerHTML = "the exponential form is " +z;
+
+}
+
